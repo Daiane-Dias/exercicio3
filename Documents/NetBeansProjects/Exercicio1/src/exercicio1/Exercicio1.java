b40f36ee31972a1579e447998cfafa973c7f2762
Pereira <Pereira@Dias>
2019-12-18 12:40:41 -0200

    Commit Exercico1

---
+++ Documents/NetBeansProjects/Exercicio1/src/exercicio1/Exercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * Imprindo um texto.
+ */
+package exercicio1;
+
+/**
+ *
+ * @author Daiane Diass
+ */
+public class Exercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Ola!!!");
+       System.out.println(123);
+       System.out.println("Seja bem vindo(a)!");
+    }
+    
+}
