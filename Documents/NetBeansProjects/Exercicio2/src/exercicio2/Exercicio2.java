2dee65cb14972c44c14354db99d16699e86f278c
Pereira <Pereira@Dias>
2019-12-18 13:23:57 -0200

    Commit  Exercico2

---
+++ Documents/NetBeansProjects/Exercicio2/src/exercicio2/Exercicio2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio2;
+
+/**
+ *
+ * @author Pereira
+ */
+public class Exercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Seja bem-vindo a este programa");
+        System.out.print("O dobro de 3 è:");
+        System.out.println(3*2);
+        System.out.println("A divisão de 12 por 2 è:"+12/2);
+    }
+    
+}
