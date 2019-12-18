b0501e6dc7cb2d2880d4a9f5f75bc3929682461e
Pereira <Pereira@Dias>
2019-12-18 13:45:56 -0200

    Revert "Commit  Exercico2"
    This reverts commit 14918735a2e77ccd50bf75a0f468e405d90739b0.

--- Documents/NetBeansProjects/Exercicio2/src/exercicio2/Exercicio2.java
+++ Documents/NetBeansProjects/Exercicio2/src/exercicio2/Exercicio2.java
@@ -1,11 +1,13 @@
 /*
- * Mostra o resultado de calculo dentro do println.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package exercicio2;
 
 /**
  *
- * @author Daiane Dias
+ * @author Pereira
  */
 public class Exercicio2 {
 
